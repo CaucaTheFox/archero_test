971e4fe506bc9923401b71d0598ddf4c45814a9b
Cauca <dragonrubyn@gmail.com>
2019-12-24 12:57:52 +0100

    [Enemies] added wave movement behavior for bats

---
+++ Assets/Scripts/Features/Enemies/EnemyBehaviours/SinusoidalBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using Core.IoC;
+using Core.Time;
+using DG.Tweening;
+using Features.Heroes;
+using UnityEngine;
+
+namespace Features.Enemies
+{
+    public class SinusoidalBehaviour : EnemyBehaviour
+    {
+        #region Unity Serialized Fields
+        [SerializeField] private float frequency, magnitude;
+        #endregion
+
+        #region Dependencies
+        #endregion
+
+        #region State
+        #endregion
+
+        #region Lifecycle
+        private void Start()
+        {
+        }
+        public override void Update()
+        {
+            MoveToTargetInWaves(); 
+        }
+        #endregion
+
+        #region Private
+        private void MoveToTargetInWaves()
+        {
+            var heroPosition = heroModel.HeroPosition;
+            heroPosition += transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
+            enemy.MoveTorwardsTarget(heroPosition);
+        }
+        #endregion
+    }
+}
