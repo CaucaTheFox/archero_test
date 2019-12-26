4e1d93336a3a6620b2a74ac7e5998a405f7dfe56
Cauca <dragonrubyn@gmail.com>
2019-12-26 21:25:27 +0100

    [Hero] shoot arrows while not moving, added rigidbody and mesh collider components

---
+++ Assets/Scripts/Features/Hero/Arrow.cs
@@ -0,0 +1,80 @@
+﻿using Features.Enemies;
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Features.Heroes
+{
+    public class Arrow : MonoBehaviour
+    {
+        #region Events
+        public event Action<Enemy> OnHitEnemy;
+        #endregion
+
+        #region Unity Serialized Fields
+        [SerializeField] private float arrowSpeed, arrowLifeTime;
+        #endregion
+
+        #region Properties
+        public Vector3 Position => transform.position;
+        public Vector3 FlightDirection { get; set; }
+        #endregion
+
+        #region State
+        private float lifeTime;
+        private bool hitObstacle;
+        #endregion
+
+        #region Lifecycle
+        private void Start()
+        {
+            lifeTime = 0;
+        }
+        private void Update()
+        {
+            if (hitObstacle)
+            {
+                return;
+            }
+
+            lifeTime += Time.deltaTime;
+            if (lifeTime >= arrowLifeTime)
+            {
+                GameObject.Destroy(gameObject);
+                return;
+            }
+
+            var movement = FlightDirection * Time.deltaTime * arrowSpeed;
+            movement.y = 0;
+            transform.position += movement;
+        }
+        private void OnDestroy()
+        {
+
+        }
+        #endregion
+
+        #region Public
+        public void OnTriggerEnter(Collider collider)
+        {
+            var obstacle = collider.gameObject.GetComponent<NavMeshObstacle>();
+            if (obstacle != null)
+            {
+                hitObstacle = true;
+                GameObject.Destroy(gameObject, 0.5f);
+                return;
+            }
+
+            var hitEnemy = collider.gameObject.GetComponent<Enemy>();
+            if (hitEnemy != null)
+            {
+                OnHitEnemy?.Invoke(hitEnemy);
+                GameObject.Destroy(gameObject);
+            }
+        }
+        #endregion
+
+        #region Private
+        #endregion
+    }
+}
--- Assets/Scripts/Features/Hero/Hero.cs
+++ Assets/Scripts/Features/Hero/Hero.cs
@@ -1,4 +1,6 @@
 ﻿using Core.IoC;
+using Core.Time;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,6 +11,7 @@ namespace Features.Heroes
         #region Unity Serialized Fields
         [SerializeField] private NavMeshAgent navMeshAgent;
         [SerializeField] private Animator animator;
+        [SerializeField] private Arrow arrowPrefab;
         #endregion
 
         #region Dependencies
@@ -19,6 +22,10 @@ namespace Features.Heroes
         public Vector3 Position => transform.position;
         #endregion
 
+        #region State
+        private Coroutine arrowInstantiationRoutine;
+        #endregion
+
         #region Lifecycle
         private void Start()
         {
@@ -26,13 +33,15 @@ namespace Features.Heroes
         }
         private void OnDestroy()
         {
-
+            StopArrowShooting();
         }
         #endregion
 
         #region Public
         public void MoveCharacter(Vector3 joyStickInput)
         {
+            StopArrowShooting();
+            
             transform.Translate(joyStickInput * navMeshAgent.speed * Time.deltaTime, Space.World);
             if (joyStickInput != Vector3.zero)
             {
@@ -43,12 +52,36 @@ namespace Features.Heroes
 
         public void Shoot()
         {
-            animator.SetTrigger("Idle");
             animator.SetTrigger("Arrow Attack");
+            if (arrowInstantiationRoutine == null)
+            {
+                arrowInstantiationRoutine = StartCoroutine(InstantiateArrows()); 
+            }
         }
         #endregion
 
         #region Private
+
+        private void StopArrowShooting()
+        {
+            if (arrowInstantiationRoutine != null)
+            {
+                StopCoroutine(arrowInstantiationRoutine);
+                arrowInstantiationRoutine = null;
+            }
+        }
+        private IEnumerator InstantiateArrows()
+        {
+            while(true)
+            {
+                yield return new WaitForSeconds(1 / Settings.AttackSpeed);
+                var arrow = GameObject.Instantiate<Arrow>(arrowPrefab);
+                arrow.transform.position = Position;
+                arrow.transform.up = -transform.forward;
+                arrow.FlightDirection = transform.forward; 
+            }
+        }
+
         #endregion
     }
 }
