1e78e96d240d0959c2df113a9bc59ce5a613fbed
Cauca <dragonrubyn@gmail.com>
2020-01-01 13:49:11 +0100

    [Combat] added random direction motion for slime

---
+++ Assets/Scripts/Features/Enemies/EnemyBehaviours/BehaviourActions/RandomDirectionMotionAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Features.Enemies
+{
+    public class RandomDirectionMotionAction : BehaviourAction
+    {
+        [SerializeField] private float directionScalar;
+
+        public override void Execute()
+        {
+            base.Execute();
+            enemyActor.SetBaseSpeed();
+            MoveInRandomDirection();
+            enemyActor.OnDestinationReached += HandleDestinationReached;
+        }
+
+        public override void Exit()
+        {
+            enemyActor.OnDestinationReached -= HandleDestinationReached;
+            base.Exit();
+        }
+        private void HandleDestinationReached()
+        {
+            MoveInRandomDirection();
+        }
+
+        private void MoveInRandomDirection()
+        {
+            var randomDirection = Random.insideUnitSphere * directionScalar;
+            randomDirection.y = 0f;
+            enemyActor.MoveTorwardsTarget(randomDirection);
+        }
+    }
+}
