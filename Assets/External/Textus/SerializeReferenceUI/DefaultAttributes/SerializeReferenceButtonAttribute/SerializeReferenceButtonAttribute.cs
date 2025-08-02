280dd4e08395c8160b91625b96211efb228a8e8e
Claudia <dragonrubyn@gmail.com>
2025-08-02 21:00:26 +0200

    [Packages] added serializeReference UI package

---
+++ Assets/External/Textus/SerializeReferenceUI/DefaultAttributes/SerializeReferenceButtonAttribute/SerializeReferenceButtonAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SerializeReferenceButtonAttribute : PropertyAttribute
+{
+    public readonly bool showIndexInLabel;
+    
+    public SerializeReferenceButtonAttribute(bool showIndexInLabel = false)
+    {
+        this.showIndexInLabel = showIndexInLabel;
+    }
+}
---
+++ Assets/External/Textus/SerializeReferenceUI/DefaultAttributes/SerializeReferenceUIRestrictionExcludeTypes/SerializeReferenceUIRestrictionExcludeTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+/// None of this types or interface types are valid.
+[AttributeUsage(AttributeTargets.Field)]
+public class SerializeReferenceUIRestrictionExcludeTypes : PropertyAttribute
+{
+    public readonly Type[] Types;
+    public SerializeReferenceUIRestrictionExcludeTypes(params Type[] types) => Types = types;
+} 
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SerializeReferenceUIRestrictionExcludeEqualTypes : PropertyAttribute
+{
+    public readonly Type[] Types;
+    public SerializeReferenceUIRestrictionExcludeEqualTypes(params Type[] types) => Types = types;
+} 
